066f4b84d7a60a3212c334078be2ae484da9e0c5
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-20 08:52:14 +0300

    fix: working ontology

--- src/main/java/WineOntology.java
+++ src/main/java/WineOntology.java
@@ -1,50 +1,58 @@
 import lombok.extern.slf4j.Slf4j;
-import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.reasoner.ReasonerFactory;
+import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
-import org.apache.jena.reasoner.rulesys.RDFSRuleReasonerFactory;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.apache.jena.ontology.OntModelSpec.OWL_MEM;
 
 @Slf4j
 public class WineOntology {
+    private static final String nameSpace = "http://www.nti.ru/wine-ontology/1.5.0#";
 
     public static void main(String[] args) {
         Model model = ModelFactory.createDefaultModel();
         OntModel ontModel = ModelFactory.createOntologyModel(OWL_MEM, model);
-        Resource wine = model.createResource("Wine");
-        Resource usWine1978 = model.createResource("US_Wine_1978");
-        Resource vintage = model.createResource("Vintage");
-        Resource wineMaker = model.createResource("WineMaker");
-        Resource winery = model.createResource("Winery");
 
-        List<Resource> resourceList = new ArrayList<>();
-        resourceList.add(wine);
-        resourceList.add(usWine1978);
-        resourceList.add(vintage);
-        resourceList.add(winery);
-        resourceList.add(wineMaker);
+        OntClass wine = ontModel.createClass(nameSpace + "Wine");
+        OntClass cheese = ontModel.createClass(nameSpace + "Wine");
 
-        for (Resource resource : resourceList) {
-            ontModel.createIndividual(resource);
-        }
+        Individual abrauDurso = wine.createIndividual(nameSpace + "AbrauDurso");
+        Individual usWine1974 = wine.createIndividual(nameSpace + "UsWine1974");
+        Individual BeaulieuVineyard = wine.createIndividual(nameSpace + "BeaulieuVineyard");
+        Individual dorBlue = cheese.createIndividual(nameSpace + "DorBlue");
 
-        ReasonerFactory reasonerFactory = new RDFSRuleReasonerFactory();
+        ObjectProperty redWine = ontModel.createObjectProperty(nameSpace + "RedWine");
+        ObjectProperty whiteWine = ontModel.createObjectProperty(nameSpace + "WhiteWine");
+        ObjectProperty oldCheese = ontModel.createObjectProperty(nameSpace + "OldCheese");
+        ObjectProperty youngCheese = ontModel.createObjectProperty(nameSpace + "YoungCheese");
 
-        ReasonerRegistry reasonerRegistry = ReasonerRegistry.theRegistry();
-        reasonerRegistry.register(reasonerFactory);
+        abrauDurso.addProperty(redWine,"Red");
+        BeaulieuVineyard.addProperty(whiteWine,"White");
+        usWine1974.addProperty(whiteWine, "White");
+
+        redWine.addInverseOf(oldCheese);
+        youngCheese.addInverseOf(whiteWine);
+
+        dorBlue.addProperty(youngCheese," YoungCheese");
+
+        DatatypeProperty yellowCheese = ontModel.createDatatypeProperty(nameSpace + "yellowCheese");
+        dorBlue.addProperty(yellowCheese, " YellowCheese");
+
+        wine.addProperty(redWine, "redWine");
+        wine.addProperty(whiteWine, "whiteWine");
+
+        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+        reasoner.bindSchema(model);
+        reasoner.supportsProperty(oldCheese);
 
         try {
-            OutputStream outputStream = new FileOutputStream("src/main/resources/wine_example_v1.rdf");
+            OutputStream outputStream = new FileOutputStream("src/main/resources/wine_and_cheese_v1.owl");
             ontModel.write(outputStream, "RDF/XML-ABBREV");
             outputStream.close();
         } catch (IOException e) {
